86f1cf7afcfacad9ceb7387292b61e0c7048f3a7
Prabhu <prabhunimi88@gmail.com>
2023-12-20 21:08:51 +0530

    changed

--- Assignments/C/convert.c
+++ Assignments/C/convert.c
@@ -1,31 +1,18 @@
 //WRITE A PROG TO  CONVERT THE GIVEN ALPHABET INTO IS OPPOSITE CASE USING BITWISE OPERATOR AND ALSO ARTHAMETIC OPERATION "IF IT'S NON-ALPHABET DISPLAY ERROR MESSAGE AS 'GIVEN CHAR IS NON-ALPHAABET'".
 
 #include<stdio.h>
-int main()
-{
+int main() {
     char ch;
-    int x;
 
     printf("Enter the Alphabet : ");
-    scanf("%c",&ch);
+    scanf("%c", &ch);
 
-
-    if (ch>=65&&90>=ch)
-    {
-        ch |= 1<<5;
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+        ch ^= (1 << 5); // Toggle the 5th bit to convert case
+        printf("%c\n", ch);
+    } else {
+        printf("PLEASE CHECK THE GIVEN CHAR IS NON-ALPHABET\n");
     }
-    else if (ch>=97&&122>=ch)
-    {
-        // ch &= (1<<5);
-        ch ^= ~(1<<5);
-    }
-    else
-    {
-        printf("PLEASE CHECH THE GIVEN CHAR IS NON-ALPHAABET");
-    }
-
-    printf("%c\n",ch);
-
 
 /*
     if (ch>=65&&90>=ch)
@@ -45,6 +32,7 @@ int main()
         printf("PLEASE CHECH THE GIVEN CHAR IS NON-ALPHAABET");
     }
 */  
-    
-    
+
+    return 0;
+
 }
