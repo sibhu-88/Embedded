1efba2e60cda31f6fdc21ce26038c32df28abefa
Prabhu <123748142+prabhu-88@users.noreply.github.com>
2024-01-02 09:40:34 +0530

    Create prime_2.c

---
+++ Assignments/C/prime_2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int prime_num(int);
+int main()
+{
+    int i=1,j,count=0;
+
+    while(count!=100)
+    {
+      for (j = 2; j < i; j++)
+      {
+        if ((i%j)==0)
+            break;
+       }
+
+       if (j==i)
+       {
+       printf("%d, ",j);
+       count++;
+       }
+       i++;
+     }
+	printf("/n");
+}
