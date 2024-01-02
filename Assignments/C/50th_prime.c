82856f34f9e66f6135b5c87e405df1cdc4bfb772
Prabhu <123748142+prabhu-88@users.noreply.github.com>
2024-01-02 09:41:14 +0530

    Create 50th_prime.c

---
+++ Assignments/C/50th_prime.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int prime_num(int);
+int main()
+{
+    int i=1,j,count=0;
+
+    while(count!=51)
+    {
+      for (j = 2; j < i; j++)
+      {
+        if ((i%j)==0)
+            break;
+       }
+
+       if (j==i)
+       {
+            count++;
+		if(count==50)
+{
+       		printf("%d, ",j);
+}
+       }
+       i++;
+     }
+        printf("\n");
+}
+
