d07937cd64c93d5db746ee60e2fcd32267717b41
Prabhu <123748142+prabhu-88@users.noreply.github.com>
2024-01-02 09:38:05 +0530

    Create prime_range.c

---
+++ Assignments/C/prime_range.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int prime_num(int);
+int main()
+{
+    int num,num1,i;
+
+    printf("Enter two values : ");
+    scanf("%d%d",&num,&num1);
+
+    if(num<num1){
+    for(i=num;i<=num1;i++)
+    prime_num(i);}
+    else if(num>num1){
+    for(i=num;i>=num1;i--)
+    prime_num(i);}
+
+    printf("\n");
+}
+
+int prime_num(int x)
+{
+    int i;
+    for (i = 2; i < x; i++)
+    {
+        if ((x%i)==0)
+        break;
+    }
+	if(i==x)
+	printf("%d, ",x);
+
+}
