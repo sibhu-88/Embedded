2a56b5f195d7e7b533a28e15310ff2e0e0d0c9b0
Prabhu <123748142+prabhu-88@users.noreply.github.com>
2024-02-19 17:55:22 +0530

    Update bank.c

--- C/Projects/Basic_Project/Bank/bank.c
+++ C/Projects/Basic_Project/Bank/bank.c
@@ -1,51 +1,56 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
 
-#define pf printf;
-#define sf scanf;
-
-void welcom_page();
-// create_acc();
-// delete_acc();
-// creadite();
-// debite();
-// bln_check();
-// alter();
+void welcome_page();
+void create_acc();
+void delete_acc();
+void creadite();
+void debite();
+void bln_check();
+void alter();
 
 int main()
 {
-    welcom_page();
+	welcome_page();
+	create_acc();
 
 }
 
-void welcom_page()
+void welcome_page()
 {
-    system("clear");
-    pf(" ░██╗░░░░░░░██╗███████╗██╗░░░░░░█████╗░░█████╗░███╗░░░███╗███████╗ ");
-    pf(" ░██║░░██╗░░██║██╔════╝██║░░░░░██╔══██╗██╔══██╗████╗░████║██╔════╝ ");
-    pf(" ░╚██╗████╗██╔╝█████╗░░██║░░░░░██║░░╚═╝██║░░██║██╔████╔██║█████╗░░ ");
-    pf(" ░░████╔═████║░██╔══╝░░██║░░░░░██║░░██╗██║░░██║██║╚██╔╝██║██╔══╝░░ ");
-    pf(" ░░╚██╔╝░╚██╔╝░███████╗███████╗╚█████╔╝╚█████╔╝██║░╚═╝░██║███████╗ ");
-    pf(" ░░░╚═╝░░░╚═╝░░╚══════╝╚══════╝░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚══════╝ ");
-
-    pf("\n");
-
-    pf("\t\t████████╗░█████╗░ ");
-    pf("\t\t╚══██╔══╝██╔══██╗ ");
-    pf("\t\t░░░██║░░░██║░░██║ ");
-    pf("\t\t░░░██║░░░██║░░██║ ");
-    pf("\t\t░░░██║░░░╚█████╔╝ ");
-    pf("\t\t░░░╚═╝░░░░╚════╝░ ");
-
-    pf("\n");
-
-
-    pf("\t░██████╗██████╗░    ██████╗░░█████╗░███╗░░██╗██╗░░██╗ ");
-    pf("\t██╔════╝██╔══██╗    ██╔══██╗██╔══██╗████╗░██║██║░██╔╝ ");
-    pf("\t╚█████╗░██████╔╝    ██████╦╝███████║██╔██╗██║█████═╝░ ");
-    pf("\t░╚═══██╗██╔═══╝░    ██╔══██╗██╔══██║██║╚████║██╔═██╗░ ");
-    pf("\t██████╔╝██║░░░░░    ██████╦╝██║░░██║██║░╚███║██║░╚██╗ ");
-    pf("\t╚═════╝░╚═╝░░░░░    ╚═════╝░╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝ ");
-
+	system("clear");
+	printf("\n\n\t\t\tHi, There Welcome to Our SP BANK\n\n");
+	sleep(2.5);
 }
 
+void create_acc()
+{
+	char f_name[20], l_name[20], dob[20], location[20], p_number[10];
+	printf("\tEnetr Your Detailes Here\n\n");
+	printf("\t\tfirst name     : ");
+	scanf("%s",f_name);
+	printf("\t\tlast name      : ");
+	scanf("%s",l_name);
+	printf("\t\tDate of Birth  : ");
+	scanf("%s",dob);
+	printf("\t\tLocation       : ");
+	scanf("%s",location);
+	printf("\t\tPhone number   : ");
+	scanf("%s",p_number);
+
+	printf("\n\n\t\tThank For the Creating Account Our Bank\n\n");
+	printf("\n\tHere you Filled Your details\n\n");
+	sleep(0.5);
+	printf("\tName           : %s %s\n",f_name,l_name);
+	printf("\tDate Of Birth  : %s\n",dob);
+	printf("\tLocation       : %s\n",location);
+	printf("\tPhone Number   : %s\n",p_number);
+
+printf("\n");
+}
+void delete_acc(){}
+void creadite(){}
+void debite(){}
+void bln_check(){}
+void alter(){}
