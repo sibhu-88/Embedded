3f823fbaac373bedd45d8188d4c17900b16393d9
Prabhu <123748142+prabhu-88@users.noreply.github.com>
2024-02-24 13:29:00 +0530

    Update sev_seg.c

--- ARM7/Embedded-c/sev_seg.c
+++ ARM7/Embedded-c/sev_seg.c
@@ -10,8 +10,6 @@
 U8 SEG_LUT[]={0XC0, 0XF9, 0XA4, 0XB0, 0X99, 0X92, 0X82, 0XF8, 0X80, 0X90}
 
 void Display_digit(int);
-void COUNT(void);
-void TIMER(void);
 
 int main()
 {
@@ -47,42 +45,4 @@ void Display_digit(int n)
 	IOCLR0 = SEG_4;			
 	delay_milisec(5);
 	IOSET0 = SEG_4;
-}
-
-void COUNT(void)
-{
-    int i,j,k,l;
-    IODIR0 = SEV_SEG | SEG_1 | SEG_2 | SEG_3 | SEG_4;
-
-    for ( i = 0; i <= 9; i++)
-    {
-			IOCLR0 = SEV_SEG;
-			IOSET0 = (SEG_LUT[i]<<2);
-			IOCLR0 = SEG_1;
-        for ( j = 0; j <= 9; j++)
-        {
-					IOCLR0 = SEV_SEG;
-					IOSET0 = (SEG_LUT[j]<<2);
-					IOCLR0 = SEG_2;
-            for ( k = 0; k <= 9; k++)
-            {
-							IOCLR0 = SEV_SEG;
-							IOSET0 = (SEG_LUT[k]<<2);
-							IOCLR0 = SEG_3;
-                for ( l = 0; l <= 9; l++)
-                {
-                    IOCLR0 = SEV_SEG;
-                    IOSET0 = (SEG_LUT[l]<<2);
-                    IOCLR0 = SEG_4;
-                    delay_milisec(250);
-                    IOSET0 = SEG_4;
-                }               
-            }
-        }
-    }
-}
-
-void TIMER(void)
-{
-
 }
