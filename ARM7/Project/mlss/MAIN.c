8b636aedabafeb2b6bbf6497a8a1401f3f955a64
Prabhu <123748142+prabhu-88@users.noreply.github.com>
2024-03-25 18:20:34 +0530

    Update MAIN.c

--- ARM7/Project/mlss/MAIN.c
+++ ARM7/Project/mlss/MAIN.c
@@ -6,11 +6,13 @@
 #define SW 15
 #define READPIN(VAR,BIT) ((VAR >> BIT)&1) 
 
+U8 P[4],G_OTP[4];
 int main(){
 	LCD_INIT();
 	init_i2c();
 	UART_INIT();
 	
+    T1TCR = 0X01;
 	UART_STR("MULTILEVEL SECURITY SYSTEM\r\n");
 	LCD_STR("MULTILEVEL SECURITY SYSTEM");
  	while(READPIN(IOPIN0,SW)==1);
