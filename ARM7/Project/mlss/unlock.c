b70f51fc65f761d7ac9a50f71af68a5d44f43ea6
Prabhu <123748142+prabhu-88@users.noreply.github.com>
2024-03-25 18:19:50 +0530

    Update unlock.c

--- ARM7/Project/mlss/unlock.c
+++ ARM7/Project/mlss/unlock.c
@@ -3,8 +3,6 @@
 #include "gsm.h"
 #include "keypad.h"
 
-#define LED1 1<<17
-#define LED2 1<<18
 #define BUZZER 1<<21
 #define M1 1<<5
 #define M2 1<<6
@@ -12,17 +10,21 @@
 U8 P[4],G_OTP[4];
 int x;
 void UNLOCK(){
-	int i=0,j=0,k=0,L=0,PASSWORD[4];
-	char E_PASSWORD[4],OTP[4],key=0,E_OTP[4];
-	IODIR0 |= LED1 | LED2 | BUZZER | M1 | M2;
+	int i=0,k=0,L=0;
+	char E_PASSWORD[4],key=0,E_OTP[4];
+	IODIR0 |= BUZZER | M1 | M2;
+
 	PASSWORD:
+	i2c_eeprom_page_write(I2C_EEPROM_SA,0X00,"1234",4);
+	i2c_eeprom_seq_read(I2C_EEPROM_SA,0x00,P,4);
+
 	LCD_CMD(0X01);
 	LCD_STR("ENTER PASSWORD");
 	UART_STR("ENTER PASSWORD\r\n");
 	LCD_CMD(0XC0);
 	
 	i=0;
-	key=KEYSCAN();
+//	key=KEYSCAN();
 	while((key=KEYSCAN())!=':')
 	{
 		UART_TX(key);
@@ -30,33 +32,34 @@ void UNLOCK(){
 		LCD_DATA('*');		
 	}
 	E_PASSWORD[i]='\0';
-	key=KEYSCAN();
-//	UART_STR("\r\n");
-//	UART_STR(E_PASSWORD);
-	
-  i2c_eeprom_page_write(I2C_EEPROM_SA,0X00,"1234",4);
-	i2c_eeprom_seq_read(I2C_EEPROM_SA,0x00,P,4);
+//	key=KEYSCAN();
+	UART_STR("\r\n");
+	UART_STR(E_PASSWORD);
+
 //UART_STR("\r\n");
 //	UART_STR(P);
-	
+	delay_milisec(1000);
 	if((strcmp(E_PASSWORD,P))==0){
+
 		GSM(G_OTP);
+
 		ENTER_OTP:
 		LCD_CMD(0X01);
 		LCD_STR("ENTER OTP");
 		UART_STR("\r\nENTER OTP\r\n");
-	  LCD_CMD(0XC0);
-		j=0;
-		key=KEYSCAN();
+	  	LCD_CMD(0XC0);
+
+		i=0;
+//		key=KEYSCAN();
 		while((key=KEYSCAN())!=':')
 		{
-			E_OTP[j++]=key;
+			E_OTP[i++]=key;
 			UART_TX(key);
 			LCD_DATA('*');
 		}
-		E_OTP[j]='\0';
-		key=KEYSCAN();
-//		
+		E_OTP[i]='\0';
+//		key=KEYSCAN();
+	
 //		UART_STR("\r\n");
 //		UART_STR(E_OTP);
 //		UART_STR("\r\n");
@@ -69,20 +72,25 @@ void UNLOCK(){
 			LCD_CMD(0X01);
 			LCD_STR("Door OPENING");
 			UART_STR("\r\nDoor OPENING\r\n");
+
 			IOSET0 = BUZZER |M1;
 			IOCLR0 =M2;
 			delay_sec(2);
 			IOCLR0 = BUZZER;
 			IOCLR0 = M1|M2;
-			delay_sec(5);
+
+			delay_sec(3);
+
 			LCD_CMD(0X01);
 			LCD_STR("Door CLOSING");
 			UART_STR("\r\nDoor CLOSING\r\n");
+
 			IOSET0 = M2|BUZZER;
 			IOCLR0 = M1;
 			delay_sec(2);
 			IOCLR0 = BUZZER;
 			IOCLR0 = M1|M2;
+
 			delay_sec(2);
 			LCD_CMD(0X01);
 			LCD_STR("THANK YOU");
@@ -99,8 +107,8 @@ void UNLOCK(){
 				while(L==3)
 				{
 					LCD_CMD(0X01);
-					LCD_STR("GO AWAY YOUR WRONG PERSION");
-					UART_STR("GO AWAY YOUR WRONG PERSION\r\n");
+					LCD_STR("ACCESS DENIED");
+					UART_STR("ACCESS DENIED\r\n");
 					while(1);
 				}
 		}
