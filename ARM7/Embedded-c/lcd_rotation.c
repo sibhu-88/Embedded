4111d3acd1d4d50121fd98d0b7f9fdee2761cffe
prabhu-88 <prabhunimi88@gmail.com>
2024-04-15 19:48:11 +0530

    modified

---
+++ ARM7/Embedded-c/lcd_rotation.c
@@ -0,0 +1,34 @@
+#include <LPC21XX.H>
+#include <string.h>
+#include "A:\Siva\Embedded\ARM7\Header\4bit_lcd.h"
+
+int main()
+{
+    LCD_Init();
+    LCD_Rotation("SIVA");
+}
+
+void LCD_Rotation(U8 *R)
+{
+    int l = strlen(R) - 1, i;
+    while (1)
+    {
+        for (i = 0; i <= 16; i++)
+        {
+            LCD_Cmd(0x01);
+            LCD_Cmd(0x80 + i);
+            LCD_Str(R);
+            delay_milisec(500);
+        }
+
+        for (i = 0; l > 0; i++, l--)
+        {
+            LCD_Cmd(0x01);
+            LCD_Cmd(0x80 + i);
+            LCD_Str(R);
+            LCD_Cmd(0x80);
+            LCD_Str(R + 1);
+            delay_milisec(500);
+        }
+    }
+}
