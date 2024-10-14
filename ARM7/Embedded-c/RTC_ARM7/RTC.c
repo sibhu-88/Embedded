ed5c2fb883fe361c6f4c7936f9db2ef7d54597e1
Prabhu <123748142+sibhu-88@users.noreply.github.com>
2024-10-14 14:50:54 +0530

    Update RTC.c

--- ARM7/Embedded-c/RTC_ARM7/RTC.c
+++ ARM7/Embedded-c/RTC_ARM7/RTC.c
@@ -3,16 +3,7 @@
 #include "4bit_lcd.h"
 #include "typedef.h"
 
-void RTC_Init(void) {
-    CCR = 0X01;
-}
-
-void RTC_SetTime(U32 hour, U32 minute, U32 second) {
-	HOUR = hour;
-	MIN =  minute;
-	SEC = second;
-	//    CTIME0 = (hour << 16) | (minute << 8) | second;
-}
+#define LED 1<<17
 
 void RTC_GetTime(U32 *hour, U32 *minute, U32 *second) {
 
@@ -21,12 +12,24 @@ void RTC_GetTime(U32 *hour, U32 *minute, U32 *second) {
     *second = SEC;
 }
 
+void RTC_SetTime(U32 hour, U32 minute, U32 second) {
+	HOUR = hour;
+	MIN =  minute;
+	SEC = second;
+}
+
+void RTC_Init(void) {
+    CCR = 0X00;
+	RTC_SetTime(12, 30, 0);
+	CCR |= 0X01;
+}
+
 int main() {
     U32 hour, minute, second;
 
     RTC_Init();
 	LCD_INIT();
-    RTC_SetTime(12, 30, 0);
+	IODIR0 = LED;
 
     while (1) {
         RTC_GetTime(&hour, &minute, &second);
@@ -38,6 +41,7 @@ int main() {
 		LCD_INTEGER(second);
 		delay_milisec(1000);
 		LCD_CMD(0X01);
+		IOPIN0 ^= LED;
     }
 }
 
